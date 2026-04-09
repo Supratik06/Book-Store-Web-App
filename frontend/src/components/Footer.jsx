79057f55ac3ea56e7336ad87b2f8d3138ea674a0
Supratik Barma <supratik.barma23@gmail.com>
2026-04-10 01:03:21 +0530

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div className="d-flex justify-content-center align-items-center p-3 text-white bg-dark" style={{borderTop:"1px solid white"}}><h5>Developed By Supratik</h5></div>
-  );
-};
-
-export default Footer
