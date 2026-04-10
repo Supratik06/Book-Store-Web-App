28f8fc8f24f7d371656cd6bc04531f7ac50189f1
Supratik Barma <supratik.barma23@gmail.com>
2026-04-10 11:03:38 +0530

    Update conn.js

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://coder:suman12345@cluster0.f5ynyxz.mongodb.net/Bookstore?retryWrites=true&w=majority&appName=Cluster0"
-).then(()=> console.log("Connected")).catch((err) => console.log("MongoDB connection failed:", err.message));
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("Connected")).catch((err) => console.log("MongoDB connection failed:", err.message));
